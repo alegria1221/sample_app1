00adcc3cf3f5fd8161e0b112a27ef3d004da0efc
alegria1221 <alegria1221@gmail.com>
2013-10-31 15:56:21 +0200

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -32,6 +33,8 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+
+  
 
     describe "when email format is invalid" do
     it "should be invalid" do
@@ -106,5 +109,10 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+  describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
 
 end
